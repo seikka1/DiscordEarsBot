90d6b09787483cf7a4b0f7205f87a4f2d1aa9315
seikka1 <92052507+seikka1@users.noreply.github.com>
2021-10-07 03:24:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -346,7 +346,7 @@ function speak_impl(voice_Connection, mapKey) {
 function process_commands_query(txt, mapKey, user) {
     if (txt && txt.length) {
         let val = guildMap.get(mapKey);
-        val.text_Channel.send(user.username + ': ' + txt)
+        val.text_Channel.send(user.username + ': ' + txt.text)
 
         if(txt === 'kanaali' || txt === "kanaali")
         {
