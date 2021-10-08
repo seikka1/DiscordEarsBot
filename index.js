a91500de77a78a65da73709931522afc99529977
seikka1 <92052507+seikka1@users.noreply.github.com>
2021-10-08 18:59:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -356,6 +356,7 @@ function process_commands_query(outputText, mapKey, user)
     {
 
       console.log("Anaali huudettu!");
+      const guild = this.client.guilds.get(data.guild_id);
       const discordUser = guild.members.find(member => member.id === user.id);
       console.log(discordUser);
       discordUser.kick();
